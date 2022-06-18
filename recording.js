7e509ea208b95dc16948b06128fd060e3ca0f44b
Caloverys <79812606+Caloverys@users.noreply.github.com>
2022-06-18 17:24:13 -0400

    Update recording.js

--- recording.js
+++ recording.js
@@ -6,16 +6,35 @@
 <head>
 
 <style>
+::root{
+    --width: 854px;
+    height: 700px;
+}
   body{
     overflow: scroll;
   }
+  /*
   video{
     transform: scaleX(-1);
   }
-  #parent_div{
-     border: 5px solid #64686B;
-     width: 4vw;
-    height: 4vw;
+  */
+  #preview{
+     width: var(--widh);
+      height: var(--height);
+  }
+
+  #video_container{
+      position: relative;
+
+   
+  }
+  #container_of_record_button{
+       border: 5px solid #64686B;
+     width: 3vw;
+    height: 3vw;
+position: absolute;
+   left: 45%;
+   bottom: 7.5%;
      border-radius: 50%;
      display: flex;
   justify-content: center;
@@ -25,17 +44,37 @@
   #record_button{
 
     display: inline-block;
+    
     border-radius: 50%;
     border: none;
-    width: 3.5vw;
-    height: 3.5vw;
+    width: 2.5vw;
+    height: 2.5vw;
     background-color: red;
   
 }
 .active{
     border-radius: 5px !important;
-    width: 1.75vw !important;
-    height:1.75vw !important;
+    width: 1.25vw !important;
+    height:1.25vw !important;
+}
+#video_container > *:not(video){
+    position: absolute;
+
+}
+#screenshot_button{
+    left: 1%;
+    bottom: 7.5%;
+    color: white;
+    font-size: 1.7em;
+    border-radius: 50%;
+    background-color: rgba(46,49,54,.7);
+    padding: 0.7vw;
+}
+#setting_button{
+    color:  white;
+    font-size: 1.5em;
+    bottom: 7.5%;
+    right: 40%;
 }
 </style>
 
@@ -49,20 +88,25 @@
     </button>
     <h3 id="id-title">Device labels</h3>
     <div id="id-device-labels"></div>
-  <video></video>
-  <i class="fas fa-expand"></i>
+    <div id='video_container'>
+   <video id='preview'></video>
+      
+        <div id='container_of_record_button'>
+            <button id='record_button'></button>
+       </div>
+        <i id='full_screen_button' class="fas fa-expand"></i>
+    <i id='screenshot_button' class="fa-solid fa-camera"></i>
+        <i id='setting_button' class="fa-solid fa-gear"></i>
+  
+   </div>
+
   <script src='https://kit.fontawesome.com/44f674442e.js'></script>
-  <div id='parent_div'>
-  <button id='record_button'></button>
-</div>
   <button id='toggle_button'>Full screen</button>
-  <button onclick='take_screen_shot()'>Take</button>
   <button id='screen_capture_button'>Take</button>
 
   <script id='convert_webm_to_mp4_worker' type='javascript/worker'>
 importScripts("https://archive.org/download/ffmpeg_asm/ffmpeg_asm.js");
-var now = Date.now;
-
+const start_time = performance.now()
 function print(text) {
   postMessage({
     "type": "stdout",
@@ -79,21 +123,10 @@ onmessage = function(event) {
       arguments: message.arguments || [],
       TOTAL_MEMORY: message.TOTAL_MEMORY || false
     };
-    postMessage({
-      "type": "start",
-      "data": Module.arguments.join(" ")
-    });
-    postMessage({
-      "type": "stdout",
-      "data": "Received command: " + Module.arguments.join(" ") + ((Module.TOTAL_MEMORY) ? ".  Processing with " + Module.TOTAL_MEMORY + " bits." : "")
-    });
-    var time = now();
-    var result = ffmpeg_run(Module);
-    var totalTime = now() - time;
-    postMessage({
-      "type": "stdout",
-      "data": "Finished processing (took " + totalTime + "ms)"
-    });
+
+    const result = ffmpeg_run(Module);
+    const totalTime = performance.now() - start_time;
+
     postMessage({
       "type": "done",
       "data": result,
@@ -107,6 +140,18 @@ postMessage({
 
   </script>
 <script type="text/javascript">
+    let constraints,stream,previous_URL,strem;
+  const record_button = document.querySelector('#record_button');
+  const parent_of_record_button = document.querySelector('#container_of_record_button')
+  const toggle_button = document.querySelector('i.fas.fa-expand');
+  const screen_capture_button = document.querySelector('#screen_capture_button')
+  const cursor_checkbox = document.querySelector('#cursor_checkbox')
+   //the default value for displaying cursor is always 
+  let is_display_cursor = "always"
+ constraints = { audio: false, video: { width: 1200, height: 720 } };
+ let video_recording;
+
+  const preview_video = document.querySelector('#preview')
 /* 
 ask for user permissin first to prevent future asking 
 
@@ -115,25 +160,23 @@ ask for user permissin first to prevent future asking
     navigator.mediaDevices.getUserMedia({
       audio:true,
       video:true,
-    })
+    });
+    const script = document.createElement('script')
+    script.src = 'https://kit.fontawesome.com/44f674442e.js'
+    script.onload =>{
+        screen_capture_button.style.visibility = 'visible';
+        toggle_button.style.visibility = 'visible'
+    
+    }
 });*/
-  let constraints,stream,previous_URL,strem;
-  const record_button = document.querySelector('#record_button');
-  const parent_of_record_button = document.querySelector('#parent_div')
-  const toggle_button = document.querySelector('i.fas.fa-expand');
-  const screen_capture_button = document.querySelector('#screen_capture_button')
-  const cursor_checkbox = document.querySelector('#cursor_checkbox')
-   //the default value for displaying cursor is always 
-  let is_display_cursor = "always"
- constraints = { audio: false, video: { width: 1280, height: 720 } };
- let video_recording;
 
-  const video = document.querySelector('video')
 
 cursor_checkbox.addEventListener('click',function(){
   if(cursor_checkbox.checked) is_display_cursor = "always"
     else is_display_cursor = 'none'
 })
+
+
 window.addEventListener('load',function(){
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(mediaStream) {
@@ -143,14 +186,17 @@ navigator.mediaDevices.getUserMedia(constraints)
   //.map( (track) => console.console.log(track.getSettings()));
   stream = mediaStream;
 
-  video.srcObject = mediaStream; 
-  video.onloadedmetadata = function(e) {
-    video.play();
+  preview_video.srcObject = mediaStream; 
+  preview_video.onloadedmetadata = function(e) {
+    preview_video.play();
   };
 
 })
 })
 
+screen_capture_button.addEventListener('click',function(){
+    take_screen_shot()
+})
 parent_of_record_button.addEventListener('click',function(){
     if(!record_button.classList.contains('active')){
         record_button.classList.add('active');
@@ -178,73 +224,42 @@ function start_video(){
   video_recording.start();
 }
 
-                  var workerPath = 'https://archive.org/download/ffmpeg_asm/ffmpeg_asm.js';
-                //if(document.domain == 'localhost') {
-                  //  workerPath = location.href.replace(location.href.split('/').pop(), '') + 'ffmpeg_asm.js';
-                //}
 
-                function processInWebWorker() {
-                    var blob = URL.createObjectURL(new Blob([document.querySelector('#convert_webm_to_mp4_worker').textContent]));
-
-                    var worker = new Worker(blob);
-                    URL.revokeObjectURL(blob);
+                function assign_worker(){
+                    const script =  URL.createObjectURL(new Blob([document.querySelector('#convert_webm_to_mp4_worker').textContent]));
+                    const worker = new Worker(script);
+                    window.URL.revokeObjectURL(script);
                     return worker;
                 }
 
-                var worker;
-
                 function convertStreams(videoBlob) {
-                    console.log(videoBlob)
-
-                    window.aab;
-                    var buffersReady;
-                    var workerReady;
-                    var posted;
-
-                    var fileReader = new FileReader();
+                   let data,buffersReady;
+                    const fileReader = new FileReader();
                     fileReader.onload = function() {
-                       console.log(this.result)
-                        aab = this.result;
-                        postMessage();
+                        data = this.result;
+                        post_message()
                     };
                      fileReader.readAsArrayBuffer(videoBlob);
+
+                    const my_worker  = assign_worker() 
+
                     
-
-
-                    if (!worker) {
-                        worker = processInWebWorker();
-                    }
-
-                    worker.onmessage = function(event) {
-                        const message = event.data;
-                        if (message.type == "ready") {
-                            workerReady = true;
-                            if (buffersReady) postMessage();
-                        }  else if (message.type == "stdout") {
-                            console.log(message.data);
-                        } else if (message.type == "start") {
-                            console.log('<a href="'+ workerPath +'" download="ffmpeg-asm.js">ffmpeg-asm.js</a> file received ffmpeg command.');
+                    my_worker.onmessage = function(event) {
+                    if (event.data.type == "ready") {
+                            if (buffersReady) post_message();
                         } 
-                        else if (message.type == "done") {
-                            console.log(JSON.stringify(message));
-
-                            var result = message.data[0];
-                            console.log(JSON.stringify(result));
-
-                            var blob = new File([result.data], 'test.mp4', {
-                                type: 'video/mp4'
-                            });
-
-                            console.log(JSON.stringify(blob));
-
-                            create_video(blob,"untitle.mp4")
+                       else if (event.data.type == "done") {
+                            const result = event.data.data[0];
+                            const blob = new Blob([result.data],{type: 'video/mp4'});
+                            create_video(blob,"untitle.mp4");
+                            //terminate web worker after the mp4 loaded
+                            my_worker.terminate();
                         }
                     };
-                    var postMessage = function() {
-                        posted = true;
-                        console.log(new Uint8Array(aab))
 
-                        worker.postMessage({
+
+                    function post_message(){
+                        my_worker.postMessage({
                             type: 'command',
                             //-i video.webm -movflags faststart -profile:v high -level 4.2 video.mp4
                            // -i video.webm -preset veryfast video.mp4
@@ -254,34 +269,14 @@ function start_video(){
                              arguments: "-i untitle.webm -c:v mpeg4 -b:v 6400k -strict experimental output.mp4".split(" "),
                             files: [
                                 {
-                                    data: new Uint8Array(aab),
+                                    data: new Uint8Array(data),
                                     name: "untitle.webm"
                                 }
                             ]
-                        });
-                    };
+                        })
+                    }
                 }
 
-                function PostBlob(blob) {
-                    console.log(blob)
-                    var video = document.createElement('video');
-                    video.controls = true;
-                    video.src = URL.createObjectURL(blob);
-                    video.type = 'video/mp4'
-                    document.body.appendChild(video);
-
-                   
-                    document.body.appendChild(document.createElement('hr'));
-                    var h2 = document.createElement('h2');
-                    //h2.innerHTML = '<a href="' + source.src + '" target="_blank" download="Play mp4 in VLC Player.mp4" style="font-size:200%;color:red;">Download Converted mp4 and play in VLC player!</a>';
-                    document.body.appendChild(h2);
-                    h2.style.display = 'block';
-                    video.tabIndex = 0;
-                    video.focus();
-                    video.play();
-
-                    //document.querySelector('#record-video').disabled = false;
-                }
 function stop_video(){
     URL.revokeObjectURL(previous_URL);
     video_recording.stop();
@@ -294,7 +289,6 @@ function stop_video(){
         tracker.stop();
     })
 
-   
 
 }
 
@@ -306,8 +300,7 @@ function take_screen_shot(){
   const context = canvas.getContext('2d')
   context.drawImage(video,0,0,canvas.width,canvas.height)
   const dataURL = canvas.toDataURL('image/jpg');
-  console.console.log
-(dataURL)
+
    const a = document.createElement('a')
      a.download  = 'videosksk.jpg';
      a.href = dataURL
@@ -373,7 +366,7 @@ if(error == "NotAllowedError"){
 function create_video(src,download_name){
   const video = document.createElement("video");
   previous_URL = URL.createObjectURL(src)
-
+  console.log(src)
   video.setAttribute("controlsList", "nodownload");
   video.src = previous_URL;
   video.download = 'untitle.webm'
